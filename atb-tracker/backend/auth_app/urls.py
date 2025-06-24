ae772b61b3e117bb78bf89bc908406cca5dd66fc
Barath ramesh <barath.r11a@gmail.com>
2025-06-24 21:27:23 +0530

    Update

--- atb-tracker/backend/auth_app/urls.py
+++ atb-tracker/backend/auth_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from users.views import LoginView
 
 urlpatterns = [
     path('google/', views.google_auth, name='google_auth'),
     path('verify/', views.verify_token, name='verify_token'),
     path('logout/', views.logout, name='logout'),
+    path('login/', LoginView.as_view(), name='login'),
 ] 
